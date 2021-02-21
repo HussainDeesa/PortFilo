6036c3b0d0ab15ebaae22617f4e15047cecf801c
Hussain <husain.m.deesawala52@gmail.com>
2021-02-21 17:08:40 +0530

    Hide

--- site/profile.js
+++ site/profile.js
@@ -1,14 +1,24 @@
-var slideIndex = 0;
-showSlides();
+// var slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+// }
+// // for automatically hide menu button
+ document.querySelector("#top").addEventListener("blur",function() {}); or
+$(function () {
+  $("#top").blur(function (event) {
+    var screenWidth=window.innerWidth;
+    if (screenWidth<1200 {
+      $("#top").collapse('hide');
+    }
+  });
+});
